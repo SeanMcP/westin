8973c8672a0a9805e1074273403e6060810febc7
Sean McPherson <git@seanmcp.com>
2019-11-13 11:10:29 -0500

    Read env variables from the top

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-    const [response, setResponse] = React.useState()
-    React.useEffect(() => {
-        fetch('http://localhost:3001').then(raw => raw.text()).then(res => setResponse(res))
-    }, [])
-    return (
-        <div className="App">
-            Hello from Westin Client!
-            <pre>{response}</pre>
-        </div>
-    );
+  const [response, setResponse] = React.useState();
+  React.useEffect(() => {
+    fetch(`http://localhost:${process.env.REACT_APP_SERVER_PORT}`)
+      .then(raw => raw.text())
+      .then(res => setResponse(res));
+  }, []);
+  return (
+    <div className="App">
+      Hello from Westin Client!
+      <pre>{response}</pre>
+    </div>
+  );
 }
 
 export default App;
