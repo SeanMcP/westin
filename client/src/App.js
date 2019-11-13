7ef1665e05ad04c25a9bab9cdc3ae0068b7594ad
Sean McPherson <git@seanmcp.com>
2019-11-13 07:06:03 -0500

    Hook it all together

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 function App() {
+    const [response, setResponse] = React.useState()
+    React.useEffect(() => {
+        fetch('http://localhost:3001').then(raw => raw.text()).then(res => setResponse(res))
+    }, [])
     return (
         <div className="App">
-            Hello from Westin!
-    </div>
+            Hello from Westin Client!
+            <pre>{response}</pre>
+        </div>
     );
 }
 
