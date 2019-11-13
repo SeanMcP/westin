fcb3de3a9b715d45548b588c0e32b15a9d61da30
Sean McPherson <git@seanmcp.com>
2019-11-13 07:05:57 -0500

    Add cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 const PORT = process.env.SERVER_PORT || 3001;
 
-app.get("/", (request, response) => response.send("Hello from Westin!"));
+app.get("/", (request, response) => response.send("Hello from Westin Server!"));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
